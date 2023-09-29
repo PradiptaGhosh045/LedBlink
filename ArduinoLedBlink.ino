95aea7b24374af13708bd857a6cf065efd3313d1
Pradipta Ghosh <146415350+PradiptaGhosh045@users.noreply.github.com>
2023-09-29 12:28:14 +0530

    Update ArduinoLedBlink.ino

--- ArduinoLedBlink.ino
+++ ArduinoLedBlink.ino
@@ -1,14 +1,14 @@
-const int LedPin = 13;
-int LedState = 0;
+const int LedPin = 13; // Define Led Connection to Arduino I/O Pin 13.
+int LedState = 0; 
 void setup()
 {
-  pinMode(LedPin, OUTPUT);
-  digitalWrite(LedPin, LedState);
+  pinMode(LedPin, OUTPUT); // Define LedPin (Pin 13) as a Output Pin
+  digitalWrite(LedPin, LedState); 
 }
 
 void loop()
 {  
-  LedState =~LedState;
-  digitalWrite(LedPin, LedState);// It will toggle the Led State
+  LedState =~LedState; // It will toggle the Led State
+  digitalWrite(LedPin, LedState); 
   delay(1000);  
 }
